60ece7a9ec2b27203a6378ad6df8f4cdbc610c3a
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-05-30 23:42:16 -0400

    updated the database.js with the logic that accepts some contents to the getDB async function

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -17,19 +17,28 @@ export const putDb = async (content) => {
   console.log('putDB is in the putDB database');
   const todosDb = await openDB('jate',1);
   const transmit = todosDb.transaction('jate', 'readwrite');
-  const store = store.put({
+  const storeObj = storeObj.put({
     value: content
   });
-  const store = transmit.objectStore('jate');
-  const req = store.put({
+  const storeObj = transmit.objectStore('jate');
+  const req = storeObj.put({
     value: content
   });
-  const result = await request;
-  console.log('😳🔥: saved to DB', result);
+  const res = await req;
+  console.log('😳🔥: saved to DB', res);
   // //console.error('putDb not implemented');
-}
-
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('get all the data from the database');
+  const pingDb = await openDB('jate', 1);
+  const transmit = pingDb.transaction('jate', 'readonly');
+  const storeObj = transmit.objectStore('jate');
+  const req = storeObj.getAll();
+  const res = await req;
+  console.log('res.value', res);
+  return res;
+  // // console.error('getDb not implemented');
+}
 
 initdb();
