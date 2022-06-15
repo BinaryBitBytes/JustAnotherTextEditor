7b4a45ffae3f70b8d6344a0bee22461bbcdb3952
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-14 23:57:54 -0400

    final commit

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -6,7 +6,7 @@ const initdb = async () =>
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
-      }
+      };
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
@@ -14,31 +14,36 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('putDB is in the putDB database');
-  const todosDb = await openDB('jate',1);
-  const transmit = todosDb.transaction('jate', 'readwrite');
-  // // const storeObj = storeObj.put({
-  // //   value: content
-  // // });
-  const storeObj = transmit.objectStore('jate');
-  const req = storeObj.put({
-    value: content
-  });
-  const res = await req;
-  console.log('😳🔥: saved to DB', res);
-  // //console.error('putDb not implemented');
+  console.log('Post to the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
 };
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('get all the data from the database');
-  const pingDb = await openDB('jate', 1);
-  const transmit = pingDb.transaction('jate', 'readonly');
-  const storeObj = transmit.objectStore('jate');
-  const req = storeObj.getAll();
-  const res = await req;
-  console.log('res.value', res);
-  return res;
-  // // console.error('getDb not implemented');
-}
+  console.log('GET from the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,27 +1,30 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
+   
     butInstall.classList.toggle('hidden', false);
 });
 
+
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const awaitEvent = window.deferredPrompt;
-    if (!awaitEvent) {
-        return;
-    }
+    const promptEvent = window.deferredPrompt;
 
-    awaitEvent.prompt();
+  if (!promptEvent) {
+   return;
+  }
 
-    window.deferredPrompt = null;
-
-    butInstall.deferredPrompt = ('hidden', true);
+  promptEvent.prompt();
+  
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', true)
 });
